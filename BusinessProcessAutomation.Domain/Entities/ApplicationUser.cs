071185834faf3cc6d0344a3edbad2bdad35754ed
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-11-15 18:07:42 +0545

    entity desgined and install identity

---
+++ BusinessProcessAutomation.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BusinessProcessAutomation.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<long>
+    {
+        public string? LinkedInUrl { get; set; }
+        public string? GitHubUrl { get; set; }
+        public string? Comment { get; set; }
+        public string? TimeInterval { get; set; }
+    }
+}
