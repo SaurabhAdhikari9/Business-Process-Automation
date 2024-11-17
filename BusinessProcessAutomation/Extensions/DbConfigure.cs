8cd1a6017861ffc706038462d758a5deee00d454
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-11-17 12:34:42 +0545

    remove identity and migration assembly

--- BusinessProcessAutomation/Extensions/DbConfigure.cs
+++ BusinessProcessAutomation/Extensions/DbConfigure.cs
@@ -8,7 +8,7 @@ namespace BusinessProcessAutomation.Extensions
         public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ApplicationDbContext>(opts =>
-                opts.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection"), b => b.MigrationsAssembly("BusinessProcessAutomation.Infrastructure")));
+                opts.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
         }
     }
 }
