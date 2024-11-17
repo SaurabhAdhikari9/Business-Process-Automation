54ad609ea39fc6d674685042eebfe3b9ef8b8730
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-11-17 19:32:18 +0545

    empty string validation added

--- BusinessProcessAutomation.Application/Common/DTOs/UserDTOs.cs
+++ BusinessProcessAutomation.Application/Common/DTOs/UserDTOs.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace BusinessProcessAutomation.Application.Common.DTOs
 {
     public class AddOrUpdateUserDTO
     {
+        [Required(AllowEmptyStrings = false)]
         public required string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public required string LastName { get; set; }
         public string? PhoneNumber { get; set; } = string.Empty;
         [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage ="Invalid email address.")]
@@ -13,6 +16,7 @@ namespace BusinessProcessAutomation.Application.Common.DTOs
         public string? TimeInterval { get; set; } = string.Empty;
         public string? LinkedInUrl { get; set; } = string.Empty;
         public string? GitHubUrl { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings =false)]
         public required string Comment { get; set; }
     }
 
