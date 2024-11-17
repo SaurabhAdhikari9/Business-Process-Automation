3048e44f5cc3a45a952d0f44fdfb1dc01fb5eaa8
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-11-17 09:44:59 +0545

    repository base setup

---
+++ BusinessProcessAutomation.Application/Interface/IRepositories/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace BusinessProcessAutomation.Application.Interface.IRepositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Create (T entity);
+        void Update (T entity);
+        void Delete (T entity);
+        void SaveChanges();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ BusinessProcessAutomation.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using BusinessProcessAutomation.Application.Interface.IRepositories;
+using BusinessProcessAutomation.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessProcessAutomation.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
