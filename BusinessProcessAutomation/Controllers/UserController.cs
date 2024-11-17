0a6e817afbbf3d801cbad090c0c1676ede52ce1c
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-11-17 17:35:53 +0545

    created user controller

---
+++ BusinessProcessAutomation/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessProcessAutomation.Application.Common.DTOs;
+using BusinessProcessAutomation.Application.Interface.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessProcessAutomation.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPut]
+        public IActionResult AddOrUpdateUser(AddOrUpdateUserDTO user)
+        {
+            _userService.AddOrUpdateUser(user);
+            return Ok(user);
+        }
+    }
+}
